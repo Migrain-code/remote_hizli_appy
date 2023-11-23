ac4085dfde9073ae7a190937c3fbb44b604c457e
migrain.code <migrain.code@gmail.com>
2023-11-23 05:35:21 +0300

    -- setup status eklendi

--- app/Http/Controllers/Api/PersonalController.php
+++ app/Http/Controllers/Api/PersonalController.php
@@ -184,7 +184,6 @@ class PersonalController extends Controller
         $business = Business::find($user->business_id);
 
         $personel = Personel::find($request->personel_id);
-        return $personel;
         $personel->name = $request->name;
         $personel->image = "business/team.png";
         $personel->email = $request->email;
