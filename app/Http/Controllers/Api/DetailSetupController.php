e12911051d35273a27c88baf38b8ff0ec848db38
migrain.code <migrain.code@gmail.com>
2023-12-17 02:36:36 +0300

    -- setup status eklendi

--- app/Http/Controllers/Api/DetailSetupController.php
+++ app/Http/Controllers/Api/DetailSetupController.php
@@ -194,10 +194,10 @@ class DetailSetupController extends Controller
         $user = $request->user()->business;
 
         if ($request->hasFile('images')){
-            return $user->gallery->count();
             if ($user->gallery->count() == 1){
                 $response = UploadFile::uploadFile($request->file('images'), 'business_wallpaper');
                 $user->wallpaper = $response["image"]["way"];
+                $user->save();
                 $gallery = new BusinessGallery();
                 $gallery->business_id = $user->id;
                 $response = UploadFile::uploadFile($request->file('images'), 'businessGallery');
