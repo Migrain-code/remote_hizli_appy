ee53cfe80641a28918984cd7219862f015f61217
migrain.code <migrain.code@gmail.com>
2024-01-06 15:16:07 +0300

    -- randevu oluşturma apisi eklendi

--- app/Http/Controllers/AppointmentCreateController.php
+++ app/Http/Controllers/AppointmentCreateController.php
@@ -81,7 +81,7 @@ class AppointmentCreateController extends Controller
             $ap_services[] = [
                 'id' => $id,
                 'title' => $service->subCategory->getName() . " için personel seçiniz",
-                'personels' => AppointmentPersonelResource::collection($servicePersonels),
+                'personels' => AppointmentPersonelResource::collection(collect($servicePersonels)),
             ];
         }
         return response()->json($ap_services);
