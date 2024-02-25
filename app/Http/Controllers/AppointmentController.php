07245b78d51edb59276757fadf202dc21dceb77f
migrain.code <migrain.code@gmail.com>
2024-02-25 23:11:17 +0300

    --randevular listesi kontrol

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -20,7 +20,7 @@ class AppointmentController extends Controller
     {
         $user = $request->user();
         $business = $user->business;
-        dd($business->appointments()->whereDate('start_time', $request->appoinment_date));
+        dd($business->appointments()->whereDate('start_time', $request->appoinment_date)->get());
         return response()->json(AppointmentResource::collection($business->appointments()->whereDate('start_time', $request->appoinment_date)->get()));
     }
 
