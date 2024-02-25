dfcca6fd88be6ac67e1f1c747f0026d40b64b02d
migrain.code <migrain.code@gmail.com>
2024-02-25 22:14:28 +0300

    -- api projesine rota dönüşüm yapıldı

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -48,34 +48,6 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind('Illuminate\Routing\ResourceRegistrar', function () use ($registrar) {
             return $registrar;
         });
-        $request = Request::instance();
-        if (!$request->is('api/*')) {
-            $settings = [];
-
-            foreach (Setting::all() as $item) {
-                $settings[$item->name] = $item->value;
-            }
-            $sections = [];
-            foreach (ForBusiness::all() as $item) {
-                $sections[$item->name] = $item->value;
-            }
-            \Config::set('sections', $sections);
-            \Config::set('settings', $settings);
-            $globalData = [
-                'pages' => Page::where('status', 1)->take(5)->latest()->get(),
-                'infos' => BusinessInfo::where('status', 0)->get(),
-            ];
-
-            \View::share('globalData', $globalData);
-            $cities = City::orderBy('name')->get();
-            View::share('cities', $cities);
-            $businesses = Business::all();
-            View::share('businesses', $businesses);
-            $services = ServiceCategory::all();
-            View::share('services_top', $services);
-            $categorys = Category::all();
-            View::share('categories', $categorys);
-        }
 
         Paginator::useBootstrapFour();
     }
