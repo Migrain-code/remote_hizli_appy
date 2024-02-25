258312444b5983d3d6db1cfd90a024188261ed6b
migrain.code <migrain.code@gmail.com>
2024-02-25 21:43:32 +0300

    -- saatler apisi düzenlendi

--- app/Models/Personel.php
+++ app/Models/Personel.php
@@ -43,6 +43,21 @@ class Personel extends Authenticatable
         return $this->hasMany(AppointmentServices::class, 'personel_id', 'id');
     }
 
+    public function stayOffDays()
+    {
+        return $this->hasOne(PersonelStayOffDay::class, 'personel_id', 'id');
+    }
+    public function checkDateIsOff($getDate)
+    {
+        // stayOffDays ilişkisini kullanarak izin tarihlerini alıyoruz.
+        $offDays = $this->stayOffDays;
+
+        if ($getDate >= $offDays->start_time && $getDate <= $offDays->end_time) {
+            return true;
+        }
+        // Eğer tarih izin tarihleri arasında değilse,false döndürüyoruz.
+        return false;
+    }
     protected static function booted()
     {
         static::deleted(function ($personel) {
