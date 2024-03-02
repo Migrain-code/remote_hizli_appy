4dad096d11c2a1d59abb146cb85a8bef19fcb8b6
migrain.code <migrain.code@gmail.com>
2024-03-02 18:44:32 +0300

    -- one signal deneme

--- app/Http/Controllers/Adission/AdissionProductSaleController.php
+++ app/Http/Controllers/Adission/AdissionProductSaleController.php
@@ -97,7 +97,87 @@ class AdissionProductSaleController extends Controller
             ]);
         }
     }
+    /**
+     * Adisyon Ürün Satışı Düzenleme
+     *
+     * Adisyonda ürün satışı düzenlemesi için gerekli bilgileri döndürür
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function edit(ProductSales $productSale)
+    {
+        $paymentTypes = ProductSales::PAYMENT_TYPES;
 
+        return response()->json([
+            'products' => ProductResource::collection($this->business->products),
+            'personels' => PersonelListResource::collection($this->business->personels),
+            'paymentTypes' => $paymentTypes,
+            'sale_detail' =>ProductSaleListResource::make($productSale)
+        ]);
+    }
+
+    /**
+     * Adisyona Ürün Satışı Güncelleme
+     *
+     * @requires product_id
+     * @requires personel_id
+     * @requires payment_type_id
+     * @requires amount
+     * @requires price
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Appointment $adission
+     * @return \Illuminate\Http\JsonResponse|void
+     */
+    public function update(ProductSaleAddRequest $request, Appointment $adission, ProductSales $productSale)
+    {
+        $saleProduct = $productSale->product; //satışı yapılan ürünü bul
+        $saleProduct->piece += $productSale->piece; //satışı yapılan ürünü güncellemeden önce eski stok adedine çevir
+        $saleProduct->save();
+
+        $productFind = Product::find($request->input('product_id'));
+        $newAmount = $productFind->piece - $request->input('amount');
+        if($newAmount < 0){
+            return response()->json([
+                'status' => "error",
+                'message' => "Satışını Yapmaya Çalıştığınız Ürünün Stoğu Yetersiz. Ürüne Stok Eklemesi Yaparak Satışı Gerçekleştirebilirsiniz"
+            ]);
+        }
+
+        $productSale->product_id = $request->input('product_id');
+        $productSale->personel_id = $request->input('personel_id');
+        $productSale->payment_type = $request->input('payment_type_id');
+        $productSale->piece = $request->input('amount');
+        $productSale->total = $this->sayiDuzenle($request->input('price')) * $request->input('amount');
+        $productSale->note = $request->input('note');
+        $productSale->created_at = $adission->start_time;
+        if ($productSale->save()) {
+            $productFind->piece = $productFind->piece - $productSale->piece;
+            $productFind->save();
+            return response()->json([
+                'status' => "success",
+                'message' => "Ürün Satışı Güncellendi"
+            ]);
+        }
+    }
+
+    /**
+     * Adisyon Ürün Satışı Silme
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Appointment $adission
+     * @return \Illuminate\Http\JsonResponse|void
+     */
+    public function destroy(ProductSales $productSale)
+    {
+        $saleProduct = $productSale->product; //satışı yapılan ürünü bul
+        $saleProduct->piece += $productSale->piece; //satışı yapılan ürünü silmeden önce eski stok adedine çevir
+        $saleProduct->save();
+
+        $productSale->delete();
+        return response()->json([
+            'status' => "success",
+            'message' => "Ürün Satışı Silindi"
+        ]);
+    }
     function sayiDuzenle($sayi){
         $sayi = str_replace('.','',$sayi);
         $sayi = str_replace(',','.',$sayi);
