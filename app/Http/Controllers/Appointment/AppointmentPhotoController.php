150bf7e4fa0b74fea43b26d41d5a55a3e4454273
migrain.code <migrain.code@gmail.com>
2024-03-16 09:56:21 +0300

    -- fotoğraflar döküman düzenlendi

--- app/Http/Controllers/Appointment/AppointmentPhotoController.php
+++ app/Http/Controllers/Appointment/AppointmentPhotoController.php
@@ -10,7 +10,7 @@ use App\Services\UploadFile;
 use Illuminate\Http\Request;
 
 /**
- * @group Randevu Fotoğraflar
+ * @group Personel Randevular
  *
  */
 class AppointmentPhotoController extends Controller
