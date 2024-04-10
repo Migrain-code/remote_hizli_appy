0b690e4a49aa348a7fdf55e7d4b99f9d05810e6a
migrain.code <migrain.code@gmail.com>
2024-04-11 00:18:12 +0300

    -- şartlar ve koşullar eklendi

--- app/Http/Controllers/Subscription/SubscribtionController.php
+++ app/Http/Controllers/Subscription/SubscribtionController.php
@@ -30,7 +30,7 @@ class SubscribtionController extends Controller
     public function index()
     {
         //$business = $this->business;
-        $terms = setting("appy_terms");
+        $terms = "<h2>Hello,</h2><p>When you’re done bundling, you should decide on the order of the topics your article. In most cases, you can decide to order thematically. For instance, if you want to discuss various aspects or angles of the main topic of your blog post. But you can also order your text chronologically or didactically.</p><p><br></p><blockquote>Buraya alıntı alanı gelecek</blockquote><p><br></p><p><br></p><p>In the above example we’re discussing, ordering topics thematically makes the most sense.</p><p>Than you,</p><p>Jerry</p>";
         return response()->json([
             'remaining_day' => now()->diffInDays($this->business->packet_end_date),
             'package' => BusinessPackageResource::make($this->business->package),
