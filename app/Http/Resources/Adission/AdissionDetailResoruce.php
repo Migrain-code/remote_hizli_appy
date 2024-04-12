7d48a859416d373261f517e183ae23b3af00b8c1
migrain.code <migrain.code@gmail.com>
2024-04-12 19:30:02 +0300

    -- yetkili şifre kontrolü yapıldı

--- app/Http/Resources/Adission/AdissionDetailResoruce.php
+++ app/Http/Resources/Adission/AdissionDetailResoruce.php
@@ -26,29 +26,13 @@ class AdissionDetailResoruce extends JsonResource
             'note' => $this->note,
             'isCampaign' => isset($this->campaign_id),
             'total' => $total,
-            'campaignDiscount' => $this->calculateCampaignDiscount(),//kampanya indirimi
-            'cashPoint' =>  $this->point,//kullanılan cash point
-            'collectedTotal' => $this->calculateCollectedTotal(),//tahsil edilecek tutar
-            'remaining_amount' => $this->remainingTotal(),//kalan tutar
-            'earningPoint' => $this->earned_point, //kazanılan parapuan
+            'campaignDiscount' => formatPrice($this->calculateCampaignDiscount()),//kampanya indirimi
+            'cashPoint' =>   formatPrice($this->point),//kullanılan cash point
+            'collectedTotal' => formatPrice($this->calculateCollectedTotal()),//tahsil edilecek tutar
+            'remaining_amount' => formatPrice($this->remainingTotal()),//kalan tutar
+            'earningPoint' => formatPrice($this->earned_point), //kazanılan parapuan
             'isPermission' => $this->earned_point > 0, //parapuan görünürlük durumu
             'payments' => AdissionPaymentListResoruce::collection($this->payments) //tahsilatlar
         ];
-    }
-    public function calculateCampaignDiscount(){ //indirim tl dönüşümü
-        $total = calculateTotal($this->services) + $this->sales->sum("total");
-        $discounttotal = number_format(($total * $this->discount) / 100, 2);
-        return $discounttotal;
-    }
-    public function calculateCollectedTotal() //tahsil edilecek tutar
-    {
-        $total = calculateTotal($this->services) + $this->sales->sum("total");
-        $recTotal = ceil($total - ((($total * $this->discount) / 100) + $this->point));
-        return $recTotal;
-    }
-
-    public function remainingTotal() //kalan tutar
-    {
-        return ($this->calculateCollectedTotal() - $this->payments->sum("price")) - $this->receivables()->whereStatus(1)->sum('price');
     }
 }
