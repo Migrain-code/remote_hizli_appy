41ba8f97c60b2aada0ff6950e6fd90c6218bac91
migrain.code <migrain.code@gmail.com>
2024-04-12 16:37:39 +0300

    -- işletme kategoroileri düzenlendi

--- app/Models/BusinessCategory.php
+++ app/Models/BusinessCategory.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class BusinessCategory extends Model
 {
-    use HasFactory;
+    use HasFactory, HasTranslations;
     protected $translatable = ['name', 'slug', 'meta_title', 'meta_description'];
 
     public function getName()
