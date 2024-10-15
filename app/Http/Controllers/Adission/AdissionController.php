8076117671a2ac94e964d09f6bc94e922d98aa34
migrain.code <migrain.code@gmail.com>
2024-10-15 12:36:53 +0300

    -- personel ekleme hizmet payı kaldırıldı

--- app/Http/Controllers/Adission/AdissionController.php
+++ app/Http/Controllers/Adission/AdissionController.php
@@ -62,9 +62,9 @@ class AdissionController extends Controller
             if (!$request->filled('start_date') && !$request->filled('end_date')) {
                 $request->merge(['date_range' => now()->format('d.m.Y') . ' - ' . now()->format('d.m.Y')]);
             } else {
-                /*$request->merge([
+                $request->merge([
                     'date_range' => Carbon::parse($request->start_date)->format('d.m.Y') . ' - ' . Carbon::parse($request->end_date)->format('d.m.Y')
-                ]);*/
+                ]);
             }
         }
     }
